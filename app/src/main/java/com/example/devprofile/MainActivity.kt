914ec189c72684589058121970dadc889160d985
prooheckcp <vasco.soares.2001@gmail.com>
2020-11-28 20:25:36 +0000

    Added tablet layout
    ^^^^

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Make the profile pic rounded
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 30f
+        findViewById<ImageView>(R.id.logo).setImageDrawable(rounded)
+
     }
 }
